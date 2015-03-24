177bd6e12f3714d18ff9c35a6a300f6b643f74a4
Matthew Kemnetz <kemnetz.m@gmail.com>
2015-03-23 20:25:11 -0400

    Working on homework

--- Spring 2015/Controls/Homework 6/Example/positioning_sys_params.m
+++ Spring 2015/Controls/Homework 6/Example/positioning_sys_params.m
@@ -1,12 +1,12 @@
 %% Evaluation of positioning system design example
- 
-P = zpk([-2 -100 roots([1 7.108 1263])'], [0 -4 -43 -43 roots([1 12 1600])'], 700);
+ close all;
+ P = zpk([-2 -100 roots([1 7.108 1263])'], [0 -4 -43 -43 roots([1 12 1600])'], 700);
 
  lead_zero = -115;   % rad/sec
  lead_pole = -914;  % rad/sec
  lead_k = 46*lead_pole/lead_zero; % N/rad
  
- act_limit = 10;    % V
+ act_limit = 60;    % V
  noise_amp = 0; %0.005;   % mm
  noise_freq = 300*2*pi; % rad/sec
  
@@ -14,7 +14,8 @@ P = zpk([-2 -100 roots([1 7.108 1263])'], [0 -4 -43 -43 roots([1 12 1600])'], 70
  
  load Reference.mat
  time = 0.001:0.001:2;
- Ref = sm_step(1751:3750);
+%  Ref = sm_step(1751:3750);
+ Ref = generateTriangeWave(time, 1/2, 3);
  
  sim pos_sys_model
  
